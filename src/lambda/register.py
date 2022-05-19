70093354c79cc128b23fa2474a3bcb0d7d09e50c
Drisya Mathilakath <drisyamathilakath@gmail.com>
2022-05-19 23:40:34 +0530

    Initial Checkin

--- src/lambda/register.py
+++ src/lambda/register.py
@@ -8,9 +8,22 @@
 # @author Anirudh Kushwah
 # @since 2022.05
 #
-from utils import respond
+from utils import *
 
 
 def handler(event, context):
-    print(event)
+    body = get_request_body_json(event)
+    print(body)
+
+    db_driver: DatabaseDriver = get_db_driver()
+    db_driver.create_user_record(user=body)
     return respond(200, "hello world", {})
+
+body = {"username":"drisya"}
+event = {
+  "isBase64Encoded": False,
+  "headers": {
+  }
+}
+event["body"] = json.dumps(json.dumps(body))
+print(handler(event, None))
--- src/user-client/app.py
+++ src/user-client/app.py
@@ -7,4 +7,23 @@
 #
 # @author Anirudh Kushwah
 # @since 2022.05
-#
+# @Update Drisya - 18.05.2022
+import json
+
+import requests
+import json
+
+class UserRegistration:
+    URL = "https://8wbn03xbll.execute-api.us-east-1.amazonaws.com/v1/register"
+    headers = {"Content-Type": "application/json"}
+    data = {}
+
+    def user_registration(self, username):
+        data = {"username": username}
+        print(json.dumps(json.dumps(data)))
+        response = requests.request("POST", self.URL, headers=self.headers, data=data)
+        print(response.text)
+
+
+user = UserRegistration()
+user.user_registration("drisya")
